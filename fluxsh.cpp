521aa4eb981a3ac1f416968cfd893778d3e1f04d
rei-root <seytumerov_rusya@mail.ru>
2026-03-10 20:51:59 +0300

    added RAM parsing

--- fluxsh.cpp
+++ fluxsh.cpp
@@ -1,16 +1,8 @@
 #include <cstdlib>
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <unistd.h>
-
-std::string RAM_usage(){
-    std::ifstream meminf("/proc/meminfo");
-    std::string line;
-    while(std::getline(meminf, line)){
-        if(line.find("") == 0 || line.find("") == 0) return line;
-    }
-}
+#include "system_monitor/system_monitor.hpp"
 
 int main(){
     std::string command;
@@ -18,9 +10,11 @@ int main(){
     char cwd[1024];
     getcwd(cwd, sizeof(cwd));
 
-
-    std::cout << name << " <~> " << "[RAM 1.5GB || CPU 8%] " << cwd << "\n";
+while(true){
+    std::cout << name << " <~> " << "[RAM " << RAM_usage() << " || " << "CPU " << "" << "] " << cwd << "\n";
     std::cout << "> ";
     getline(std::cin, command);
     system(command.c_str());
+}
+
 }
---
+++ system_monitor/system_monitor.cpp
@@ -0,0 +1,29 @@
+#include "system_monitor.hpp"
+#include <fstream>
+
+
+
+std::string RAM_usage(){
+    std::ifstream meminf("/proc/meminfo");
+    std::string line;
+    long total = 0, available = 0;
+    if(!meminf.is_open()){
+        throw std::runtime_error("Error: can't open meminfo");
+    }
+    while (std::getline(meminf, line)) {
+        if(line.find("MemTotal: " == 0)){
+            sscanf(line.c_str(), "MemTotal: %ld", &total);
+        }
+        if(line.find("MemAvailable: " == 0)){
+            sscanf(line.c_str(), "MemAvailable: %ld", &available);
+        }
+    }
+
+    if(total == 0){return "0/0kb";}
+
+    long RAM_usage_Kb = total - available;
+
+    double RAM_usage_Gb = RAM_usage_Kb / 1024.0 / 1024.0;
+
+    return std::to_string(RAM_usage_Gb).substr(0, 4) + "Gb";
+}
